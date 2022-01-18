8ef73a233415af9a1dbd935611f55ef71801a3ff
rsgarcia0203 <rsgarcia@espol.edu.ec>
2022-01-18 11:44:18 -0500

    TestMovie

---
+++ RentalSys/RentalSystem/src/test/java/com/espol/rentalsystemtest/MovieTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.espol.rentalsystemtest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rsgar
+ */
+public class MovieTest {
+    
+    public MovieTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getPriceCode method, of class Movie.
+     */
+    @Test
+    public void testGetPriceCode() {
+        System.out.println("getPriceCode");
+        int rd = (int)Math.random();
+        final Movie movieTest = new Movie("test", rd);
+        movieTest.setPriceCode(2);
+        assertTrue(movieTest.getPriceCode() == rd);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPriceCode method, of class Movie.
+     */
+    @Test
+    public void testSetPriceCode() {
+        System.out.println("setPriceCode");
+        int rd = (int)Math.random();
+        final Movie movieTest = new Movie("test", rd);
+        movieTest.setPriceCode(2);
+        assertTrue(movieTest.getPriceCode() == rd);
+        // TODO review the generated test code and remove the defwault call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
